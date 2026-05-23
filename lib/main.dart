2b6d1ee5b94f9679f1133a69fb66d06164553e3b
Shashika Pathum Dilhara <161570319+PathumDilhara@users.noreply.github.com>
2026-05-23 14:51:27 +0530

    Initial Project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "SMS Finance App For Flutter Intern Assessment",
+      home: Scaffold(appBar: AppBar(title: Text("SMS Finance App"))),
+    );
+  }
+}
